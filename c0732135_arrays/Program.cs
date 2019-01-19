04d643387466981ae2705623a1dfea64fcb95df3
visualstudio <visualstudio@122-PC08>
2019-01-18 20:40:58 -0500

    Added the code for stuinfo code

--- c0732135_arrays/Program.cs
+++ c0732135_arrays/Program.cs
@@ -10,6 +10,31 @@ namespace c0732135_arrays
     {
         static void Main(string[] args)
         {
+            
+            StudentInformation Mary=new StudentInformation ("Mary",3.8,20);
+            StudentInformation John = new StudentInformation("John", 2.0, 10);
+            StudentInformation Mark = new StudentInformation("Mark", 0.2, 3);
+            StudentInformation Steve = new StudentInformation("Steve", 2.9, 16);
+            StudentInformation Cindy = new StudentInformation("Cindy", 3.9, 187);
+
+
+
+
+        }
+    }
+    class StudentInformation
+    {
+       public string name;
+        
+        public double gpa;
+        public int Attendance;
+        public StudentInformation(string aname, double agpa, int aAttendance)
+        {
+            name = aname;
+
+            gpa = agpa;
+
+            Attendance = aAttendance;
         }
     }
 }
